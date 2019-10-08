bd9fe0e3be2a60f9a3a36da1ddb63e1d2093f969
leo ouyang <Leo Ouyang>
2019-10-07 22:02:36 -0400

    Added appropriate error handling so the program doesn

--- main.py
+++ main.py
@@ -33,15 +33,18 @@ def saveData(sides,chi,pvalue,rollData):
     file.write("D"+str(sides) + " Test\n")
     now = datetime.datetime.now()
     file.write("Created: %d/%d/%d\n" %( now.day, now.month, now.year))
-    print(chi, pvalue)
     file.write("Chi-squared value: %f\n" %chi)
     file.write("P-value: %f\n" %pvalue)
     file.write("Actual data: \n")
     for i in range(len(rollData)):
         file.write("%d: %d\n" %(i+1,rollData[i]))
     print("Saved to ", fileName + ".txt")
-    
-sides = int(input("Welcome to dice tester! How many sides does your dice have?"))
+sides = 0 #number of sides
+while sides == 0:
+    try:
+        sides = int(input("Welcome to dice tester! How many sides does your dice have?"))
+    except ValueError:
+        continue
 rollData = [] #how much we rolled each number
 for i in range(sides):
     rollData.append(0)
@@ -50,15 +53,19 @@ rolls = 0 #the number of rolls
 rolled = 0 #the number rolled
 
 print("Roll the dice and type the roll here. For this dice, it is recommended to roll at least", 5*sides,"times")
-typed = input("Roll the dice or type \"done\" to stop" ) #input
+typed = "" #get input
 while typed != "done":
     try:
+        typed = input("Roll the dice or type \"done\" to stop. Rolls: " + str(rolls) + "\n")
         rolled = int(typed)
-    except TypeError:
+        rollData[rolled-1] += 1
+    except ValueError:
+        print("Input wasn't a valid number. Try again")
         continue
-    rollData[rolled-1] += 1
+    except IndexError:
+        print("Input wasn't a rollable number. Try again")
+        continue
     rolls += 1
-    typed = input("Roll the dice or type \"done\" to stop. Rolls: " + str(rolls) + "\n")
     
 chi = 0
 expected = rolls/sides
@@ -68,9 +75,11 @@ if expected != 0:
         chi += pow(i-expected,2)/expected 
     pvalue = pValue(df,chi) #calculate p-value
 print("Your chi-squared value is:", chi, "and your p-value is:", pvalue)
-answer = input("Would you like to save this data in a txt file?").lower()
+answer = input("Would you like to save this data in a txt file? (\"yes\" or \"no\")").lower()
 if answer == "y" or answer == "yes" or answer == "yeah":
     saveData(sides,chi,pvalue,rollData)
+else:
+    print("Data not saved")
 input("Press any enter to quit")
 
     
